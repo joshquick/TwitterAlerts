0dabe6d68fe3d0d279a551867d944214eda2a560
joshquick <j.quick@bham.ac.uk>
2016-01-27 13:05:42 +0000

    Add alert script

---
+++ tweet_alert.py
@@ -0,0 +1,34 @@
+from twitter import *
+import sys
+import datetime
+
+
+def tweet():
+
+	t = Twitter(auth=OAuth(token, token_key, con_secret_key, con_secret))
+
+	dm = datetime.datetime.now().strftime('%x %H:%M:%S') + " Mum! I'm stuck in Bangkok!!!"
+
+	t.direct_messages.new(user="scalene", text=dm
+)
+	"""
+	t.direct_messages.new(user="bioemformatics", text=dm
+)
+	"""
+	t.direct_messages.new(user="pathogenomenick", text=dm
+)
+	t.direct_messages.new(user="robot_alerts", text=dm
+)
+	t.direct_messages.new(user="masonry_stoves", text=dm
+)
+	t.direct_messages.new(user="DrTimothyWells", text=dm
+)
+
+	print 'Sent direct message... %s' %dm
+
+
+
+
+if __name__ == '__main__':
+
+	tweet()
